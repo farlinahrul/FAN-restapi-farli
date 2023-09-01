b9a668b7f87f620cdab4ebea37553fd8316dd2ab
farlinahrul <farlynj@gmail.com>
2023-09-02 02:19:00 +0700

    feat(presence): add approve route

--- routes/ApiRoutes/PresenceRoutes.php
+++ routes/ApiRoutes/PresenceRoutes.php
@@ -6,6 +6,7 @@ Route::get("presence", [PresenceController::class, 'index']);
 Route::get("my_presence", [PresenceController::class, 'myPresence']);
 Route::get("my_presence_approval", [PresenceController::class, 'myPresenceApproval']);
 Route::post("presence", [PresenceController::class, 'store']);
+Route::post("approve/{id}", [PresenceController::class, 'store']);
 
 // imporvement: pagination
 Route::get("my_presence_paginate", [PresenceController::class, 'myPresencePaginate']);
